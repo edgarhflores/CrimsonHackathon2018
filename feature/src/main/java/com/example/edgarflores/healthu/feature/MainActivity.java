6929e4e9b4038ae376b5fdfde50366b5fb5e9463
Edgar Flores <edgarflores@Edgars-MacBook.local>
2018-04-14 18:43:53 -0500

    first commit

---
+++ feature/src/main/java/com/example/edgarflores/healthu/feature/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.edgarflores.healthu.feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

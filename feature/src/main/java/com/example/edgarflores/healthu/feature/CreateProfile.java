61be2239f049f4538a2b292ecbc6b6ce0de31b96
rburks <rburks1996@gmail.com>
2018-04-15 10:05:45 -0500

    Login Page Created

---
+++ feature/src/main/java/com/example/edgarflores/healthu/feature/CreateProfile.java
@@ -0,0 +1,16 @@
+package com.example.edgarflores.healthu.feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_profile);
+
+
+    }
+
+}
--- feature/src/main/java/com/example/edgarflores/healthu/feature/MainActivity.java
+++ feature/src/main/java/com/example/edgarflores/healthu/feature/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.edgarflores.healthu.feature;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button create_profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        create_profile=(Button) findViewById(R.id.button2);
+        create_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateProfile();
+
+            }
+        });
+
+
+
+    }
+    public void openCreateProfile(){
+        Intent intent = new Intent(this, CreateProfile.class);
+        startActivity (intent);
     }
 }

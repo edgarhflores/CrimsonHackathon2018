eae83a92cc4ca3e31c50506d907abc2e5c35bd95
Edgar Flores <edgarflores@Edgars-MacBook.local>
2018-04-15 11:48:13 -0500

    added perscrpitions

--- feature/src/main/java/com/example/edgarflores/healthu/feature/MainActivity.java
+++ feature/src/main/java/com/example/edgarflores/healthu/feature/MainActivity.java
@@ -8,12 +8,22 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button create_profile;
+    private Button sign_up;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sign_up = (Button) findViewById(R.id.signUp);
+
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPrescription();
+            }
+        });
 
         create_profile=(Button) findViewById(R.id.button2);
+
         create_profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,11 +32,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
     }
     public void openCreateProfile(){
         Intent intent = new Intent(this, CreateProfile.class);
+        startActivity (intent);
+    }
+
+    public void openPrescription(){
+        Intent intent =new Intent (this, Perscription.class);
         startActivity (intent);
     }
 }
